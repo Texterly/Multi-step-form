93b2eb1463522bb874ce0a7bd0f11f4bf82be4d2
Texterly <88582978+Texterly@users.noreply.github.com>
2022-11-14 18:49:36 +0300

    add button

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Button({color, text, onClick, isDisabled}) {
+  return (
+    <button
+        className={`btn btn-${color}`}
+        onClick={onClick}
+        disabled={isDisabled}
+    >
+        {text}
+    </button>
+  );
+}
+
+export default Button;
